1e7f89b5e9a43ff2417dec30dcfb18c890af8872
Mishal Shashikant <mishalmsb@gmail.com>
2017-08-24 23:36:41 +0100

    usin counties in autocomplete
    using countries services to set automplete data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component }                 from '@angular/core';
+import { CountriesService, Country } from './services/countries';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular2 autocomplete';
+
+    title : string = 'Angular2 autocomplete';
+    countries : Country[];
+    countrCode : string;
+
+    constructor(private _countryService : CountriesService) {}
+
+    ngOnInit() {
+        this.getCountries();
+    }
+
+    getCountries() {
+        this._countryService.getCountries().subscribe((countries: Country[]) => {
+            this.countries = countries;
+        })
+    }
+
+    onChange() {
+        console.log(this.countrCode);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule }      from '@angular/platform-browser';
 import { NgModule }           from '@angular/core';
+import { FormsModule }        from '@angular/forms';
 import { AppComponent }       from './app.component';
 import { AutocompleteModule } from './components/autocomplete';
+import { CountriesService }   from './services';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AutocompleteModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AutocompleteModule
+    ],
+    providers: [
+        CountriesService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
